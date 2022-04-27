4ed4753345874477e846165bb17006feeaddc3f7
Sad_boizzzz <94224022+hungkaw2011@users.noreply.github.com>
2022-04-27 20:47:53 +0700

    Update DSMN_(DSLKD).cpp

--- Linked List/DSMN_(DSLKD).cpp
+++ Linked List/DSMN_(DSLKD).cpp
@@ -1,5 +1,3 @@
-//Author Hungka
-
 #include <iostream>
 #include<iomanip>
 #include<stdio.h>
@@ -15,16 +13,16 @@ private:
 	int NamSinh;
 	float GPA;
 public:
-    Student(string MaSV="", string HoDem="", string Ten="", string GioiTinh="", int NamSinh=0, float gpa=0)
-        : MaSV(MaSV), HoDem(HoDem), Ten(Ten), GioiTinh(GioiTinh), NamSinh(NamSinh), GPA(gpa) {}
+	Student(string MaSV = "", string HoDem = "", string Ten = "", string GioiTinh = "", int NamSinh = 0, float gpa = 0)
+		: MaSV(MaSV), HoDem(HoDem), Ten(Ten), GioiTinh(GioiTinh), NamSinh(NamSinh), GPA(gpa) {}
 	void Nhap() {
-		cout << "Nhap ma sinh vien:"; fflush(stdin); getline(cin,MaSV);
-		if (MaSV.compare( "***") == 0) {
+		cout << "Nhap ma sinh vien:"; fflush(stdin); getline(cin, MaSV);
+		if (MaSV.compare("***") == 0) {
 			return;
 		}
-		cout << "Nhap ho dem: "; fflush(stdin); getline(cin,HoDem);
-		cout << "Nhap ten: ";  fflush(stdin); getline(cin,Ten);
-		cout << "Nhap gioi tinh: ";  fflush(stdin); getline(cin,GioiTinh);
+		cout << "Nhap ho dem: "; fflush(stdin); getline(cin, HoDem);
+		cout << "Nhap ten: ";  fflush(stdin); getline(cin, Ten);
+		cout << "Nhap gioi tinh: ";  fflush(stdin); getline(cin, GioiTinh);
 		cout << "Nhap nam sinh: "; cin >> NamSinh;
 		cout << "Nhap diem: "; cin >> GPA;
 	}
@@ -35,7 +33,7 @@ public:
 		return GPA;
 	}
 	void setMaSV(string MaSV) {
-		this->MaSV=MaSV;
+		this->MaSV = MaSV;
 	}
 	string getMaSV() {
 		return MaSV;
@@ -54,38 +52,38 @@ ostream& operator << (ostream& out, Student& L) {
 
 class Node {
 public:
-    Student data;
-    Node* next;
-    Node(Student data) {
-        this->next = nullptr;
-        this->data = data;
-    }
+	Student data;
+	Node* next;
+	Node(Student data) {
+		this->next = nullptr;
+		this->data = data;
+	}
 };
 
 class LIST {
 private:
-    Node* head;
-    Node* tail;
+	Node* head;
+	Node* tail;
 public:
-    LIST() {
-        head = nullptr;
-        tail = nullptr;
-    }
+	LIST() {
+		head = nullptr;
+		tail = nullptr;
+	}
 
-    bool isEmpty() {
-        return head == nullptr;
-    }
+	bool isEmpty() {
+		return head == nullptr;
+	}
 
-    void add(Student data) {
-        Node* p = new Node(data);
-        if (isEmpty()) {
-            head = tail = p;
-        }
-        else {
-            p->next = head;
-            head = p;
-        }
-    }
+	void add(Student data) {
+		Node* p = new Node(data);
+		if (isEmpty()) {
+			head = tail = p;
+		}
+		else {
+			p->next = head;
+			head = p;
+		}
+	}
 	void NhapDS(LIST& L) {
 		Student X;
 		int i = 1;
@@ -96,32 +94,32 @@ public:
 			if (X.MaSV.compare("***") == 0) {
 				return;
 			}
-            cin.ignore();
-            addTail(X);
+			cin.ignore();
+			addTail(X);
 			i++;
 		} while (1);
 	}
-    void addTail(Student data) {
-        Node* p = new Node(data);
-        if (isEmpty()) {
-            head = tail = p;
-        }
-        else {
-            tail->next = p;
-            tail = p;
-        }
-    }
-    void showList() {
-        Node* p = head;
+	void addTail(Student data) {
+		Node* p = new Node(data);
+		if (isEmpty()) {
+			head = tail = p;
+		}
+		else {
+			tail->next = p;
+			tail = p;
+		}
+	}
+	void showList() {
+		Node* p = head;
 		int i = 0;
 		cout << setw(5) << "STT" << setw(10) << "MaSV" << setw(20) << "Ho va Ten" << setw(13) << "Nam Sinh" << setw(10) << "Diem TK" << endl;
-        while (p != nullptr) {
-            cout << setw(5) << i + 1 << p->data << endl;
-            p = p->next;
+		while (p != nullptr) {
+			cout << setw(5) << i + 1 << p->data << endl;
+			p = p->next;
 			i++;
-        }
-        cout << endl;
-    }
+		}
+		cout << endl;
+	}
 	// hàm cập nhật điểm và theo mã sinh viên
 	bool update(Student data) {
 		Node* p = head;
@@ -144,7 +142,7 @@ public:
 			p = p->next;
 		}
 	}
-	bool addAfterX(Student sv,int x) {
+	bool addAfterX(Student sv, int x) {
 		Node* p = new Node(sv); // tạo node mới p
 		Node* nodeX = head; // bắt đầu từ node head
 		int k = 1;
@@ -172,7 +170,7 @@ public:
 			}
 		}
 		return false;
-	}	
+	}
 	void removeByMaSV(string name) {
 		if (isEmpty()) {
 			cout << "Danh sach rong.\n";
@@ -204,43 +202,6 @@ public:
 			x = x->next;
 		}
 	}
-	void addAfterX_vip(LIST& L, int& x) {
-		Student sv;
-		cout << "Nhap Vi Tri Can Chen:"; cin >> x;
-		cout << "Nhap TT SV Can chen:\n";
-		cin.ignore();
-		sv.Nhap();
-		Node* p = new Node(sv); // tạo node mới p
-		Node* nodeX = head; // bắt đầu từ node head
-		int k = 1;
-		if (x ==0 || nodeX == nullptr) {
-			add(sv);
-		}
-		else{		
-			while (nodeX != nullptr && k != x ) { // tìm nodeX
-				nodeX = nodeX->next; // chuyển tới node kế tiếp
-				++k;
-			}
-			if (x>=k) {
-				k = 1;
-				while (nodeX != nullptr && k < x) { // tìm nodeX
-					nodeX = nodeX->next; // chuyển tới node kế tiếp
-					++k;
-					if (x > k) {
-						break;
-					}
-				}
-				cout << "Chen SV vao Vi tri " << x << " Khong Hop Le!!\n";
-				cout << "Vay Chen vao vi tri cuoi cung\n";
-				addTail(sv);
-			}
-			else {
-				p->data = sv;
-				p->next = nodeX->next;
-				nodeX->next = p;
-			}
-		}
-	}
 	void deleteHead() {
 		if (head == nullptr) {
 			cout << "Khong Co SV nao de xoa\n";
@@ -250,8 +211,8 @@ public:
 		}
 		//        Hoặc	C2
 		/*  Node* r = head; // lưu lại head
-		    head = head->next; // cập nhật head mới
-		    delete r; // xóa head						*/
+			head = head->next; // cập nhật head mới
+			delete r; // xóa head						*/
 	}
 	void deleteTail() {
 		//		  Cách 1 Duyệt Từ nút head đến tail  Hơi Vất =)) 
@@ -259,42 +220,48 @@ public:
 		if (head == nullptr || head->next == nullptr) { // Kiem tra DS Co 0 , 1 pt
 			deleteHead();
 		}
-		while (r->next->next !=nullptr)
+		while (r->next->next != nullptr)
 		{
 			r = r->next; // Duyet DS
 		}
 		r->next = r->next->next;
 		//        Hoặc	C2	
 		/*	Student sv;
-			Node* r = tail;  //Tro r de not cuoi 
+			Node* r = tail;  //Tro r de not cuoi
 			Node* x=new Node(sv);  // Khoi Tao Node x bat ki
-			x->next = nullptr; //Cap Nhat X tro den null 
+			x->next = nullptr; //Cap Nhat X tro den null
 			tail = x; //Gan not cuoi DS la X
 			delete r;									*/
-		
+
 	}
-	void remove(int &y) {
+	void remove(int& y) {
 		cout << "Nhap Vi Tri Xoa "; cin >> y;
 		int k = 1;
-		Node* z = head;
 		Node* nodeX = head;
-		while (nodeX != nullptr && k != y) { // tìm nodeX
-			nodeX = nodeX->next; // chuyển tới node kế tiếp
-			++k;
+		if (y == 0) {
+			cout << "Vi tri xoa k dung";
+			return;
 		}
-		if (y > k) {
-			cout << "Vi Tri Xoa K Dung Do DS chi chua " << k << " SV" << endl;
+		if (y == 1 || head == NULL || head->next == NULL) {
+			deleteHead(); 
 		}
 		else {
-			if (z == head) {
-				deleteHead();
+			int k = 1;
+			Node *p = head;
+			while (p->next->next != NULL && k != y) {
+				p = p->next;
+				++k;
 			}
-			else if (z == tail) {
+
+			if (k != y) {
+				if (y > k) {
+					cout << "Vi tri xoa vuot qua vi tri cuoi cung!";
+					return;
+				}
 				deleteTail();
 			}
-			else if (z != nullptr) {
-				nodeX->next = z->next;
-				delete z;
+			else {
+				p->next = p->next->next;
 			}
 		}
 	}
@@ -313,13 +280,13 @@ public:
 };
 int main() {
 	LIST students;
-        //Tu dong input cac gia tri
+	//Tu dong input cac gia tri
 	students.add(Student{ "ACC01", "TRAN VAN", " NAM", "Nam", 2002,2 });
 	students.add(Student{ "ACC02", "TRAN VAN", " PHONG", "Nam", 2003,2.3 });
-	cout << "Danh sach truoc update: \n";   
+	cout << "Danh sach truoc update: \n";
 	students.showList();
 	cout << "Ban Co Muon Nhap Them SV ?\nEnter *** De huy Nhap Them SV" << endl;
-        students.NhapDS(students);
+	students.NhapDS(students);
 	students.showList();
 	students.update(Student("ACC01", "", "", "", 0, 3.2));
 	cout << "Sau Khi Update Diem theo MaSV\n";
@@ -329,12 +296,10 @@ int main() {
 	cout << "Nhap Ma SV Can thay doi diem:";
 	getline(cin, MaSV);
 	cout << "Nhap Diem Thay Doi:"; cin >> Diem;
-	students.update_vip(students,MaSV,Diem);
+	students.update_vip(students, MaSV, Diem);
 	students.showList();
-	students.addAfterX({ "ACC03", "NGUYEN VAN", " HUNG", "Nam", 2001,3.1 }, 0);
-	students.showList();
-	int x,y;
-	students.addAfterX_vip(students,x);
+	students.addAfterX({ "ACC03", "NGUYEN VAN", " HUNG", "Nam", 2001,3.1 }, 1);
+	int y;
 	students.showList();
 	students.deleteHead();
 	students.showList();
